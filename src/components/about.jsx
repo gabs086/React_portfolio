5aaa87292f1e3af5443615d5bdc572e1fbc5298d
gabs086 <agoncillogabriel08@gmail.com>
2021-03-16 19:51:18 +0800

    v2, 1st init

---
+++ src/components/about.jsx
@@ -0,0 +1,212 @@
+import React from "react";
+import myImage from "../img/myImage.png";
+
+//Skills images
+import html from "../img/skills/html.png";
+import css from "../img/skills/css.png";
+import js from "../img/skills/js.png";
+import php from "../img/skills/php.png";
+import mysql from "../img/skills/mysql.png";
+import node from "../img/skills/node.png";
+import express from "../img/skills/express.png";
+import react from "../img/skills/react.png";
+import mongodb from "../img/skills/mongodb.png";
+import codeigniter from "../img/skills/codeigniter.png";
+import materialize from "../img/skills/materialize.png";
+import bootstrap from "../img/skills/bootstrap.png";
+import wordpress from "../img/skills/wordpress.png";
+import materialUi from "../img/skills/material-ui.png";
+import reactstrap from "../img/skills/reactstrap.png";
+import powerDirector from "../img/skills/powerdirector.png";
+import git from "../img/skills/git.png";
+import reactNative from "../img/skills/react-native.png";
+import sendgrid from "../img/skills/sendgrid.png";
+import android from "../img/skills/android.png";
+import mailchimp from "../img/skills/mailchimp.png";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      about_me: [
+        {
+          id: "first-p-about",
+          content:
+            "Hi, I'm Gabriel Agoncillo. I'm a software developer/engineer who loves engineering things to enhance my ability, skills, learn more and build solutions to answer someones problem through the use of technology.",
+        },
+        {
+          id: "second-p-about",
+          content:
+            "Most stack of technology that I used is the MERN stack and the LAMP stack for web development. But I'm interested in learning new things such us learning about how mobile development works. Interested in using React-native and native android development using java.",
+        },
+        {
+          id: "third-p-about",
+          content:
+            "I also have a little bit skills in marketing such using tools of Mailchimp & SendGrid. To learn more about me, Please download my CV or send me a message through the contact form below",
+        },
+      ],
+    };
+  }
+
+  componentDid
+
+  render() {
+    return (
+      <section id="about" className="about-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="box-shadow-full">
+                <div className="row">
+                  <div className="col-md-6">
+
+                  <div className="title-box-2 skills">
+                        <h5 className="title-left">My Skills</h5>
+                      </div>
+
+                    <div className="skill-mf text-center">
+                      {/* Skills to be divided by 3  */}
+
+                      <div className="row">
+
+                        <div className="col-sm-4">
+                          <img src={html} alt="HTML" width="80" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={css} alt="CSS" width="80" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={js} alt="JS" width="80" />
+                        </div>
+                      </div>
+                      {/* row */}
+
+                      <div className="row pt-4">
+                        <div className="col-sm-4">
+                          <img src={php} alt="PHP" width="80" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={mysql} alt="MySQL" width="80" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={mongodb} alt="MongoDB" width="100" />
+                        </div>
+                      </div>
+                      {/* row */}
+
+                      <div className="row pt-4">
+                        <div className="col-sm-4">
+                          <img src={express} alt="ExpressJS" width="100" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={react} alt="ReactJS" width="100" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={node} alt="NodeJS" width="100" />
+                        </div>
+                      </div>
+                      {/* row */}
+
+                      <div className="row pt-4">
+                        <div className="col-sm-4">
+                          <img src={reactNative} alt="React-Native" width="80" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={android} alt="Android studio(java)" width="80" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={wordpress} alt="Wordpress" width="80" />
+                        </div>
+                      </div>
+                      {/* row */}
+
+                      <div className="row pt-4">
+                        <div className="col-sm-4 pt-4">
+                          <img src={codeigniter} alt="CodeIgniter" width="100" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={materialize} alt="Materialize-css" width="100" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={bootstrap} alt="Bootstrap" width="100" />
+                        </div>
+                      </div>
+                      {/* row */}
+
+                      <div className="row pt-4">  
+                        <div className="col-sm-4 pt-4">
+                          <img src={materialUi} alt="Material-UI" width="100" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={reactstrap} alt="Reactstrap" width="150" height="120"/>
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={git} alt="git" width="100" />
+                        </div>
+                      </div>
+                      {/* row */}
+
+                      <div className="row pt-4">  
+                        <div className="col-sm-4 pt-4">
+                          <img src={sendgrid} alt="SendGrid" width="140" />
+                        </div>
+
+                        <div className="col-sm-4">
+                          <img src={mailchimp} alt="Mailchimp" width="100"/>
+                        </div>
+
+                      </div>
+                      {/* row */}
+
+                    </div>
+                    {/* skill-mf */}
+                  </div>
+                  {/* col-md-6 */}
+
+                  <div className="col-md-6">
+                    <div className="about-me pt-4 pt-md-0">
+                      <div className="title-box-2">
+                        <h5 className="title-left">About Me</h5>
+                      </div>
+                      {this.state.about_me.map((content) => {
+                        return (
+                          <p className="lead" key={content.id}>
+                            {content.content}
+                          </p>
+                        );
+                      })}
+                    </div>
+                    <a
+                  className="btn btn-primary btn js-scroll px-4"
+                  href="#work"
+                  role="button"
+                >
+                  <i className="fa fa-download"></i> 
+                   &nbsp;
+                  Download my CV
+                </a>
+                  </div>
+                  {/* col-md-6 */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/portfolio.jsx
@@ -0,0 +1,223 @@
+import React from "react";
+
+//import the work images
+import divisoria from '../img/works/divisoriaKing.png';
+import gtsWebsite from '../img/works/gts_corporate_website.png';
+import gtsStore from '../img/works/gts-store.png';
+import soa from '../img/works/soa-web-portal.png';
+import techniqo from '../img/works/techniqo.png';
+import zeus from '../img/works/zeus.png';
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <section id="work" className="portfolio-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Portfolio</h3>
+                <p className="subtitle-a">
+                  View my works while learning how to code and while in the
+                  software development industry
+                </p>
+                <div className="line-mf"></div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+
+            <div className="col-md-4">
+              <div className="work-box">
+                <div className="work-img">
+                  <img src={soa} alt="" className="img-fluid" />
+                </div>
+                <div className="work-content">
+                  <div className="row">
+                    <div className="col-sm-8">
+                      <h2 className="w-title">Revised version - SOA Web Portal (Thesis, Self study)</h2>
+                      <div className="w-more">
+                        <span className="w-ctegory">
+                          MERN Material-UI
+                        </span>
+                      </div>
+                    </div>
+                    <div className="col-sm-4 text-center work-view">
+                        <a
+                          className="btn btn-primary btn js-scroll px-4"
+                          href="https://soa-web-portal.herokuapp.com/"
+                          role="button"
+                          target="_blank"
+                        >
+                           View
+                        </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* col-md-4 */}
+
+            <div className="col-md-4">
+              <div className="work-box">
+                <div className="work-img">
+                  <img src={divisoria} alt="" className="img-fluid" />
+                </div>
+                <div className="work-content">
+                  <div className="row">
+                    <div className="col-sm-8">
+                      <h2 className="w-title">Divisoria King (InternShip, Contribution)</h2>
+                      <div className="w-more">
+                        <span className="w-ctegory">
+                          WordPress WooCommerce PHP
+                        </span>{" "}
+                      </div>
+                    </div>
+                    <div className="col-sm-4 text-center work-view">
+                        <a
+                          className="btn btn-primary btn js-scroll px-4"
+                          href="https://divisoriaking.com/"
+                          role="button"
+                          target="_blank"
+                        >
+                           View
+                        </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* col-md-4 */}
+
+            <div className="col-md-4">
+              <div className="work-box">
+                <div className="work-img">
+                  <img src={techniqo} alt="" className="img-fluid" />
+                </div>
+                <div className="work-content">
+                  <div className="row">
+                    <div className="col-sm-8">
+                      <h2 className="w-title">TechniQo (Internship, Develop)</h2>
+                      <div className="w-more">
+                        <span className="w-ctegory">
+                          HTML5 CSS3 JavaScript Bootstrap
+                        </span>
+                        {/*} / <span className="w-date">18 Sep. 2018</span>*/}
+                      </div>
+                    </div>
+                    <div className="col-sm-4 text-center work-view">
+                        <a
+                          className="btn btn-primary btn js-scroll px-4"
+                          href="https://techniqo.org/"
+                          role="button"
+                          target="_blank"
+                        >
+                           View
+                        </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* col-md-4 */}
+
+            <div className="col-md-4">
+              <div className="work-box">
+                <div className="work-img">
+                  <img src={gtsWebsite} alt="" className="img-fluid" />
+                </div>
+                <div className="work-content">
+                  <div className="row">
+                    <div className="col-sm-8">
+                      <h2 className="w-title">GTS Corporate Website (Company, Develop)</h2>
+                      <div className="w-more">
+                        <span className="w-ctegory">
+                        HTML5 CSS3 JavaScript PHP JotForms Bootstrap
+                        </span>
+                      </div>
+                    </div>
+                    <div className="col-sm-4 text-center work-view">
+                        <a
+                          className="btn btn-primary btn js-scroll px-4"
+                          href="https://globaltigersolutions.com/"
+                          role="button"
+                          target="_blank"
+                        >
+                           View
+                        </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="work-box">
+                <div className="work-img">
+                  <img src={gtsStore} alt="" className="img-fluid" />
+                </div>
+                <div className="work-content">
+                  <div className="row">
+                    <div className="col-sm-8">
+                      <h2 className="w-title">GTS Online Store (Company, Develop BETA version)</h2>
+                      <div className="w-more">
+                        <span className="w-ctegory">WordPress WooCommerce PHP</span>
+                      </div>
+                    </div>
+                    <div className="col-sm-4 text-center work-view">
+                        <a
+                          className="btn btn-primary btn js-scroll px-4"
+                          href="https://store.globaltigersolutions.com/"
+                          role="button"
+                          target="_blank"
+                        >
+                           View
+                        </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* col-md-4 */}
+
+            <div className="col-md-4">
+              <div className="work-box">
+                <div className="work-img">
+                  <img src={zeus} alt="" className="img-fluid" />
+                </div>
+                <div className="work-content">
+                  <div className="row">
+                    <div className="col-sm-8">
+                      <h2 className="w-title">FIDS (Company, Contribute/Develop BETA)</h2>
+                      <div className="w-more">
+                        <span className="w-ctegory">
+                          Codeigniter MERN Material-UI Android Studio (Java)
+                        </span>
+                      </div>
+                    </div>
+                    <div className="col-sm-4 text-center work-view">
+                        <a
+                          className="btn btn-primary btn js-scroll px-4"
+                          href="https://globaltigersolutions.com/products/enterprise/zeus.php"
+                          role="button"
+                          target="_blank"
+                        >
+                           View
+                        </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* col-md-4 */}
+          </div>
+
+
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
