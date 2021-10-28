da087c059578978945956ba1171acc69818b7028
gabs086 <agoncillogabriel08@gmail.com>
2021-10-28 11:10:07 +0800

    Installed react-helmet, Create Header component and Modify index.html

---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+function Header() {
+  return (
+    <Helmet>
+      <title>Gabriel V. Agoncillo | Software Developer</title>
+      <meta name='keywords' content='Gab Agoncillo, Gabriel Agoncillo, gab, gabriel, agoncillo' />
+      <meta name='author' content='Gab Agoncillo' />
+      <meta name='description' content='Software Developer/Engineer open for new career opportunities ' />
+      <meta name='robots' content='index' />
+      <link rel='canonical' href='https://gabrielagoncillo.netlify.app/' />
+    </Helmet>
+  );
+}
+
+export default Header;
