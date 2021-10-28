630e50b56310041461f031977136aa8269696db3
gabs086 <agoncillogabriel08@gmail.com>
2021-10-28 11:30:47 +0800

    Edit header tag codes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,11 +4,13 @@ import { Helmet } from 'react-helmet';
 function Header() {
   return (
     <Helmet>
-      <meta name='keywords' content='Gab Agoncillo, Gabriel Agoncillo, gab, gabriel, agoncillo' />
-      <meta name='author' content='Gab Agoncillo' />
       <meta name='description' content='Software Developer/Engineer open for new career opportunities ' />
-      <meta name='robots' content='index' />
-      <link rel='canonical' href='https://gabrielagoncillo.netlify.app/' />
+      <meta
+        name='keywords'
+        content='Gab Agoncillo, Gabriel Agoncillo, Gab Agoncillo Netlify, Gabriel Agoncillo Netlify, gab agoncillo, gabriel agoncillo, gab agoncillo netlify, gabriel agoncillo netlify, gab, gab netlify, gabriel, gabriel netlify, agoncillo'
+      />
+      <meta name='author' content='Gab Agoncillo' />
+      <meta name='robots' content='index,follow' />
     </Helmet>
   );
 }
